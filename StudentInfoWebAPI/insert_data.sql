d445672482581e473ec53879779f7845f8900aa5
Moleboheng Maine <moleboheng.maine@psg.co.za>
2025-05-26 08:03:38 +0200

    Add sql file

---
+++ StudentInfoWebAPI/insert_data.sql
@@ -0,0 +1,5 @@
+﻿ INSERT INTO [StudentInfoDb].[dbo].[Students] ([Name], [Age], [Email], [CreatedAt])
+VALUES
+    ('Jane Kruger', 20,'jane.kruger@university.ac.za', '2023-01-15'),
+    ('Jane Smith', 22,'jane.smith@university.ac.za', '2022-09-05'),
+    ('Thabo Moloisane', 21,'thabo.moloisane@university.ac.za', '2023-03-20');
